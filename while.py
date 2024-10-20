2e9c8fff752a9a12449db94e538186eaa6382568
Phonso <abdoulazizc867@gmail.com>
2024-10-20 15:54:02 +0000

    Exercise: while name is empty!

--- while.py
+++ while.py
@@ -1,3 +1,4 @@
+"""
 name = input("Quel est ton nom ? : ")
 next_age = 0
 while next_age == 0:
@@ -9,3 +10,11 @@ while next_age == 0:
         print("Erreur : L'âge est inccorecte, tu dois mettre un nombre !")
 
 print(f"Tu t'appelles {name}, ton âge est {age} ans et l'an prochain tu auras : {next_age} ans.")
+"""
+
+# use while loop when name is empty
+name = ""
+while name == "":
+    name = input("Quel est ton nom ? : ")
+
+print(f"Ton nom est: {name}")
