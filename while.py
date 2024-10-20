cdce068e5f2408686b695cd6b474d19648e16006
Phonso <abdoulazizc867@gmail.com>
2024-10-20 19:10:54 +0000

    Exercise: function !

--- while.py
+++ while.py
@@ -1,20 +1,22 @@
-"""
-name = input("Quel est ton nom ? : ")
-next_age = 0
-while next_age == 0:
 
-    try:
-        age = int(input("Quel est ton âge ? : "))
-        next_age = age + 1
-    except ValueError:
-        print("Erreur : L'âge est inccorecte, tu dois mettre un nombre !")
+def ask_age():
+    fun_age = 0
+    while fun_age == 0:
+        age_str = input("Quel est ton âge ? : ")
+        try:
+            fun_age = int(age_str)
+        except ValueError:
+            print("Erreur : L'âge est inccorecte, tu dois mettre un nombre !")
+    return fun_age
 
-print(f"Tu t'appelles {name}, ton âge est {age} ans et l'an prochain tu auras : {next_age} ans.")
-"""
 
-# use while loop when name is empty
-name = ""
-while name == "":
-    name = input("Quel est ton nom ? : ")
+def ask_name():
+    name_answer = ""
+    while name_answer == "":
+        name_answer = input("Quel est ton nom ? : ")
+    return name_answer
 
-print(f"Ton nom est: {name}")
+
+name = ask_name()
+age = ask_age()
+print(f"Ton nom est {name} et tu as {age} ans. L'an prochain tu auras " + str(age + 1) + " ans")
